02d8623f48618408ce3eaf22365f53c0c7c9319c
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-05-21 17:17:24 +0200

    Add: milestone 12

--- src/assets/pages/TaskList.jsx
+++ src/assets/pages/TaskList.jsx
@@ -1,12 +1,32 @@
-import { useMemo, useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import { useGlobalContext } from '../contexts/GlobalContetx'
 import TaskRow from '../components/TaskRow'
+
+function debounce(callback, delay) {
+    // Variabile timer per gestire il timeout
+    let timer
+
+    // Ritorna una funzione che può essere usata per eseguire il debounce
+    return ((value) => {
+        // Cancella il timeout precedente (se esiste)
+        clearTimeout(timer)
+        // Imposta un nuovo timeout che esegue la callback dopo il delay
+        timer = setTimeout(() => {
+            callback(value)
+        }, delay)
+    })
+}
 
 export default function TaskList() {
 
     const { tasks } = useGlobalContext()
     const [sortBy, setSortBy] = useState('createdAt')
     const [sortOrder, setSortOrder] = useState(1)
+    const [searchQuery, setSearchQuery] = useState('')
+
+    const filterAndOrderTask = useCallback(
+        debounce(setSearchQuery, 500),
+        [])
 
     function handleClick(element) {
 
@@ -20,9 +40,11 @@ export default function TaskList() {
         console.log(sortOrder)
     }
 
-    const ordine = useMemo(() => {
+    const ordineTask = useMemo(() => {
 
-        const copiaData = [...tasks]
+        const filtroTask = tasks.filter(e => {
+            return e.title.toLowerCase().includes(searchQuery.toLowerCase())
+        })
 
         const stato = {
             'To do': 0,
@@ -30,7 +52,7 @@ export default function TaskList() {
             'Done': 2
         }
 
-        copiaData.sort((a, b) => {
+        filtroTask.sort((a, b) => {
 
             if ('title' === sortBy) {
                 return a.title.localeCompare(b.title) * sortOrder
@@ -41,15 +63,19 @@ export default function TaskList() {
             }
 
         })
-        return copiaData
+        return filtroTask
 
-    }, [sortBy, sortOrder, tasks])
+    }, [sortBy, sortOrder, tasks, searchQuery])
 
 
     return (
         <>
             <div>
                 <h1 className='text-center'>Lista Task</h1>
+                <input type="text"
+                    placeholder='Ricerca task'
+                    onChange={e => filterAndOrderTask(e.target.value)}
+                />
             </div>
             <div>
                 <table className="table text-center">
@@ -62,7 +88,7 @@ export default function TaskList() {
                         </tr>
                     </thead>
                     <tbody>
-                        {ordine.map(task =>
+                        {ordineTask.map(task =>
                             <TaskRow key={task.id} task={task} />
                         )}
                     </tbody>
